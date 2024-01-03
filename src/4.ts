09d318de498d3fb2fc95fd84e6cd32d7477aff0c
shaman2507 <alabay174@gmail.com>
2024-01-03 15:58:45 +0100

    fixed

--- src/4.ts
+++ src/4.ts
@@ -1,9 +1,5 @@
 class Key {
-  private signature: number;
-
-  constructor() {
-    this.signature = Math.random();
-  }
+  private signature: number = Math.random();
 
   getSignature(): number {
     return this.signature;
@@ -11,10 +7,8 @@ class Key {
 }
 
 class Person {
-  private key: Key;
-
-  constructor(key: Key) {
-    this.key = key;
+  constructor(private key: Key) {
+    
   }
 
   getKey(): Key {
@@ -23,13 +17,11 @@ class Person {
 }
 
 abstract class House {
-  protected door: boolean;
-  protected key: Key;
-  protected tenants: Person[];
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
 
-  constructor(key: Key) {
+  constructor(protected key: Key) {
     this.door = false;
-    this.key = key;
     this.tenants = [];
   }
 
@@ -46,10 +38,6 @@ abstract class House {
 }
 
 class MyHouse extends House {
-  constructor(key: Key) {
-    super(key);
-  }
-
   openDoor(key: Key): void {
     if (key.getSignature() === this.key.getSignature()) {
       this.door = true;
